a19114f11f7d017b6a0661e22689b69433ae8dea
zxypp <z@xypp.cc>
2022-10-27 19:24:51 +0800

    新增首页巨幕和轮播图，导航栏沉浸，优化移动端自适应 新增历史记录功能 新增贡献分页，优化布局 回滚了贡献者字段的贡献时表现（不再自动填入原有的贡献者）

--- cerfai-frontend/src/views/HomeView.vue
+++ cerfai-frontend/src/views/HomeView.vue
@@ -1,24 +1,30 @@
 <template>
     <div class="home">
-        <el-menu :default-active="activeIndex" class="menu" mode="horizontal" @select="handleSelect">
-            <el-menu-item index="tag">Novelai词条贡献站 ></el-menu-item>
+        <el-menu :default-active="activeIndex" class="menu" mode="horizontal" @select="handleSelect" style="z-index:2"
+            :class="{ transBack: activeIndex == 'home' }">
+            <el-menu-item index="home">Novelai词条贡献站 ></el-menu-item>
+            <el-menu-item index="contribute">参与贡献</el-menu-item>
             <el-menu-item index="tag_search">搜索词条</el-menu-item>
             <el-menu-item index="contributor">贡献榜</el-menu-item>
             <el-menu-item index="share">共享计划</el-menu-item>
         </el-menu>
-
-        <el-collapse class="collapse" v-model="activeNames">
-            <el-collapse-item title="Novelai词条资源贡献站" name="1">
+        <large-image-title v-show="activeIndex == 'home'" style="position:absolute;top:0px;width: 100%;z-index: 1;"
+            :items="imageSet" title="Novelai词条贡献站" height="500">
+            <div style="height:25%;"></div>
+            <h1>Novelai词条贡献站</h1>
+            <small>收到用户贡献词条数: {{ up_cnt }},已收录总词条数: {{ ct_total }}</small><br><br>
+            <el-button type="primary" round @click="activeIndex = 'contribute'">前往贡献页面→</el-button>
+        </large-image-title>
+        <div class="container">
+            <!--TODO:文案等待修改-->
+            <div class="homePage" v-show="activeIndex == 'home'" style="position:relative;top:450px;">
+                <h3>关于本站</h3>
                 <div>
                     本站皆在建立novelai中所包含的词条解释数据库，为广大魔导师提供词条贡献平台，所有收集的数据在收集整理后进行公开，为novelai开源生态建立数据仓库基础，如果您有其他数据源或者自行整理的词条信息，并且希望参与本站建设，可以直接联系我们进行批量数据导入<br />
                     联系方式： 621816415(洛儿)<br />
                     本站交流群：660612010
                 </div>
-                <div>目前已收到用户贡献词条数: {{ up_cnt }}</div>
-                <div>当前站点已收录总词条数: {{ ct_total }}</div>
                 <div>参与贡献基本原则：在不破坏他人劳动成果的前提下，对词条原有信息做任何有效补充均可视为有效贡献</div>
-            </el-collapse-item>
-            <el-collapse-item title="贡献操作指南" name="2">
                 <div>
                     您所看到的所有词条内容来自 机器翻译/人工初筛/其他用户个人上传。
                     欢迎对词条进行内容审核或者参与实际测试，并提供宝贵的建议。
@@ -28,77 +34,96 @@
                     如果经多方测试，标签无效或者当前版本无效，请输入并选择“无效标签”
                     如果您对当前标签没兴趣或者不需要进行修改，点击“换一个”更换词条。
                 </div>
-                <div>
-                    <strong>词条名：<br /></strong>
-                    是指导入数据库的Tag英文原名，软件可识别的词条/标签。
-                </div>
-                <div>
-                    <strong>词条译名：<br /></strong>
-                    是指词条翻译的中文名，或者贡献者修改的译名，最好是中文版AI可识别的译名。因为尚未校正，可能存在翻译错误。
-                </div>
-                <div>
-                    <strong>是否NSWF：<br /></strong>
-                    当前标签生成图片时是否会出现不适合工作场合的画面，包括不限于血腥暴力和敏感内容，如果是请打开此项。
-                </div>
-                <div>
-                    <strong>词条说明：<br /></strong>
-                    对于词条的补充和内容说明。对标签进行测试时，建议标明SD测试还是NF测试，两者会有些许差异。单独使用标签与实际混合标签时会产生不同效果，cfg参数会影响标签效果，标签数字权重与{}的增加也会标签产生的效果，建议多次测试确定效果。
-                </div>
-                <div>
-                    <strong>备注信息：<br /></strong>
-                    词条的来源和调查。词条的正确译名可能需要借助谷歌等搜索引擎，容易产生误解的是画师账号昵称和人名，并且可能由于关联搜索而出现奇怪的内容，导致内容与标签毫无关联，这取决于模型的收录程度，如果要强调画师或者风格，应该加上by_或者（style）
-                </div>
-                <div>
-                    <strong>贡献者：<br /></strong>
-                    所有参与词条编辑的人都可以填入贡献者名字，请使用常用的昵称或者数字字母，提交词条将计入贡献名单，优秀的内容提交者将在榜单上体现，请尊重自己和他人的劳动成果，恶意删改将会触发网站防御，并拉入AI绘画信息并联计划黑名单。
-                </div>
-            </el-collapse-item>
-        </el-collapse>
 
-        <div class="container">
+            </div>
 
-            <!-- 贡献表单 -->
-            <el-form v-show="activeIndex == 'tag'" ref="form" :model="form" label-width="100px">
-                <el-form-item label="词条名：">
-                    <div class="row">
-                        <el-input v-model="form.name" disabled></el-input>
-                        <el-button type="primary" v-clipboard:copy="form.name" v-clipboard:success="onCopy"
-                            v-clipboard:error="onError">复制词条名</el-button>
-                    </div>
-                </el-form-item>
-                <el-form-item label="词条译名：">
-                    <el-input v-model="form.t_name"></el-input>
-                </el-form-item>
-                <el-form-item label="词条分类：">
-                    <category-selector :value="form.c_id" @change="nv => form.c_id =nv"></category-selector>
-                </el-form-item>
-                <el-form-item label="是否NSFW：">
-                    <el-switch v-model="form.is_nsfw"></el-switch>
-                    <span class="nsfw-span" style="color: #e66" v-show="form.is_nsfw">这是一个包含工作场所不宜内容的词条</span>
-                    <span class="nsfw-span" v-show="!form.is_nsfw">这是一个任何场所安全的词条</span>
-                </el-form-item>
-                <el-form-item label="词条说明：">
-                    <el-input type="textarea" v-model="form.desc" placeholder="请输入词条说明信息，作为该词条的使用参考或者效果"></el-input>
-                </el-form-item>
-                <el-form-item label="备注信息：">
-                    <el-input type="textarea" v-model="form.remarks" placeholder="请输入备注信息，可包含该词条信息依据的参考资料链接"></el-input>
-                </el-form-item>
-                <el-form-item label="贡献者：">
-                    <el-input v-model="form.contributor" placeholder="请留下您的用户名或昵称，以便我们记录贡献者，多位贡献者请用英文逗号分隔"></el-input>
-                </el-form-item>
+            <div class="contributePage" v-show="activeIndex == 'contribute'">
+                <!-- 贡献表单 -->
+                <el-row :gutter="10">
+                    <el-col :xs="24" :sm="24" :md="18" :lg="16" :xl="16">
+                        <el-form ref="form" :model="form" label-width="100px">
+                            <el-form-item label="词条名：">
+                                <div class="row">
+                                    <el-input v-model="form.name" disabled></el-input>
+                                    <el-button type="primary" v-clipboard:copy="form.name" v-clipboard:success="onCopy"
+                                        v-clipboard:error="onError">复制词条名</el-button>
+                                </div>
+                            </el-form-item>
+                            <el-form-item label="词条译名：">
+                                <el-input v-model="form.t_name"></el-input>
+                            </el-form-item>
+                            <el-form-item label="词条分类：">
+                                <category-selector :value="form.c_id" @change="nv => form.c_id = nv">
+                                </category-selector>
+                            </el-form-item>
+                            <el-form-item label="是否NSFW：">
+                                <el-switch v-model="form.is_nsfw"></el-switch>
+                                <span class="nsfw-span" style="color: #e66"
+                                    v-show="form.is_nsfw">这是一个包含工作场所不宜内容的词条</span>
+                                <span class="nsfw-span" v-show="!form.is_nsfw">这是一个任何场所安全的词条</span>
+                            </el-form-item>
+                            <el-form-item label="词条说明：">
+                                <el-input type="textarea" v-model="form.desc" placeholder="请输入词条说明信息，作为该词条的使用参考或者效果">
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item label="备注信息：">
+                                <el-input type="textarea" v-model="form.remarks"
+                                    placeholder="请输入备注信息，可包含该词条信息依据的参考资料链接">
+                                </el-input>
+                            </el-form-item>
+                            <el-form-item label="贡献者：">
+                                <el-input v-model="form.contributor" minlength="1"
+                                    placeholder="请留下您的用户名或昵称，以便我们记录贡献者，多位贡献者请用英文逗号分隔"></el-input>
+                            </el-form-item>
 
-                <el-form-item>
-                    <el-button type="primary" @click="onSubmit">提交</el-button>
-                    <el-button @click="roll_tag">换一个</el-button>
-                </el-form-item>
-            </el-form>
+                            <el-form-item>
+                                <el-button type="primary" @click="onSubmit">提交</el-button>
+                                <el-button @click="roll_tag">换一个</el-button>
+                            </el-form-item>
+                        </el-form>
+                    </el-col>
+                    <el-col :xs="24" :sm="24" :md="6" :lg="8" :xl="8">
+                        <el-card class="box-card">
+                            <div slot="header" class="clearfix">
+                                <span>贡献指南</span>
+                            </div>
+                            <div class="text item">
+                                <strong>词条名：<br /></strong>
+                                是指导入数据库的Tag英文原名，软件可识别的词条/标签。
+                            </div>
+                            <div class="text item">
+                                <strong>词条译名：<br /></strong>
+                                是指词条翻译的中文名，或者贡献者修改的译名，最好是中文版AI可识别的译名。因为尚未校正，可能存在翻译错误。
+                            </div>
+                            <div class="text item">
+                                <strong>是否NSWF：<br /></strong>
+                                当前标签生成图片时是否会出现不适合工作场合的画面，包括不限于血腥暴力和敏感内容，如果是请打开此项。
+                            </div>
+                            <div class="text item">
+                                <strong>词条说明：<br /></strong>
+                                对于词条的补充和内容说明。对标签进行测试时，建议标明SD测试还是NF测试，两者会有些许差异。单独使用标签与实际混合标签时会产生不同效果，cfg参数会影响标签效果，标签数字权重与{}的增加也会标签产生的效果，建议多次测试确定效果。
+                            </div>
+                            <div class="text item">
+                                <strong>备注信息：<br /></strong>
+                                词条的来源和调查。词条的正确译名可能需要借助谷歌等搜索引擎，容易产生误解的是画师账号昵称和人名，并且可能由于关联搜索而出现奇怪的内容，导致内容与标签毫无关联，这取决于模型的收录程度，如果要强调画师或者风格，应该加上by_或者（style）
+                            </div>
+                            <div class="text item">
+                                <strong>贡献者：<br /></strong>
+                                所有参与词条编辑的人都可以填入贡献者名字，请使用常用的昵称或者数字字母，提交词条将计入贡献名单，优秀的内容提交者将在榜单上体现，请尊重自己和他人的劳动成果，恶意删改将会触发网站防御，并拉入AI绘画信息并联计划黑名单。
+                            </div>
+                        </el-card>
 
+                    </el-col>
+                </el-row>
+            </div>
             <!-- 搜索页面 -->
             <div class="search" v-show="activeIndex == 'tag_search'">
                 <div class="row">
                     <el-input v-model="search_keyword" @keyup.enter.native="search" placeholder="请输入词条名"></el-input>
                     <el-button type="primary" icon="el-icon-search" @click="search"></el-button>
                 </div>
+                <search-history :datas="searchHis" @select="k => { this.search_keyword = k, this.search() }"
+                    @clear="clearHistory"></search-history>
                 <el-table @row-click="getRow" :data="search_res" class="searchTable" border fit style="width: 100%">
                     <el-table-column prop="name" label="词条名"></el-table-column>
                     <el-table-column prop="t_name" label="词条译名"></el-table-column>
@@ -147,13 +172,15 @@
 
 <script>
 import categorySelector from '@/components/categorySelector.vue'
+import largeImageTitle from '@/components/largeImageTitle.vue'
+import searchHistory from '@/components/searchHistory.vue'
 export default {
     name: 'Home',
     data() {
         return {
             // serverhost: 'www.morangames.xyz:3090',
             // serverhost: '127.0.0.1:3090',
-            activeIndex: 'tag',
+            activeIndex: 'home',
             form: {
                 id: '',
                 name: '',
@@ -172,9 +199,20 @@ export default {
             search_keyword: '',
             search_res: [],
             contributor_toplist: [],
+            imageSet: [
+                "images/3.jpg",
+                "images/5.jpg",
+                "images/4.jpg",
+                "images/6.jpg",
+            ],
+            searchHis: []
         }
     },
     methods: {
+        clearHistory() {
+            localStorage.searchHis = '';
+            this.searchHis = [];
+        },
         // 提交tag
         onSubmit() {
             console.log('submit!');
@@ -233,6 +271,8 @@ export default {
         },
         // 搜索tag
         search() {
+            this.searchHis.unshift({ w: this.search_keyword, i: this.searchHis.length })
+            localStorage.searchHis = JSON.stringify(this.searchHis)
             let _this = this
             // 获取categories列表
             this.$http({
@@ -246,15 +286,15 @@ export default {
                     if (res.data.code == 200) {
                         // console.log(res.data)
                         if (res.data.data.length > 0) {
-                            for (let i = 0; i < res.data.data.length;i++){
-                                let curId = parseInt(res.data.data[i].c_id),l1name="";
-                                _this.$store.state.categories.l1.forEach(elem=>{
-                                    if (elem.id == parseInt(curId / 100)) l1name=elem.name;
+                            for (let i = 0; i < res.data.data.length; i++) {
+                                let curId = parseInt(res.data.data[i].c_id), l1name = "";
+                                _this.$store.state.categories.l1.forEach(elem => {
+                                    if (elem.id == parseInt(curId / 100)) l1name = elem.name;
                                 })
-                                if (_this.$store.state.categories.l2[parseInt(curId/100)]!==undefined){
-                                    _this.$store.state.categories.l2[parseInt(curId/100)].forEach(elem=>{
-                                        if(elem.id == curId){
-                                            res.data.data[i].c_name = l1name +">"+elem.name;
+                                if (_this.$store.state.categories.l2[parseInt(curId / 100)] !== undefined) {
+                                    _this.$store.state.categories.l2[parseInt(curId / 100)].forEach(elem => {
+                                        if (elem.id == curId) {
+                                            res.data.data[i].c_name = l1name + ">" + elem.name;
                                         }
                                     })
                                 }
@@ -326,7 +366,7 @@ export default {
         },
         //获取完整的分类列表
         //TODO:该函数为过渡方案，等待替换
-        get_categories_full(){
+        get_categories_full() {
             let _this = this
             // 获取categories列表
             this.$http({
@@ -340,7 +380,7 @@ export default {
                 .catch((err) => {
                     console.log(err)
                 })
-            
+
         },
         // 复制成功时的回调函数
         onCopy(e) {
@@ -366,16 +406,19 @@ export default {
                 _this.form.is_nsfw = _this.selRowData.is_nfsw ? true : false
                 _this.form.desc = _this.selRowData.desc
                 _this.form.remarks = _this.selRowData.remarks
-                _this.form.contributor = _this.selRowData.contributor
-                _this.activeIndex = 'tag'
+                //_this.form.contributor = _this.selRowData.contributor
+                _this.activeIndex = 'contribute'
             }, 50);
 
         }
     },
+    computed: {
+    },
     mounted() {
+        this.searchHis = JSON.parse(localStorage.searchHis || "[]")
         this.get_categories()
     }, components: {
-        categorySelector
+        categorySelector, largeImageTitle, searchHistory
     }
 }
 </script>
@@ -387,6 +430,22 @@ export default {
     justify-content: center;
     align-items: center;
     box-sizing: border-box;
+
+    .transBack {
+        background-color: transparent;
+        color: white;
+        font-weight: bold;
+        border-bottom: none;
+
+        .el-menu-item.is-active {
+            color: white;
+        }
+
+        .el-menu-item:hover,
+        .el-menu-item:focus {
+            background-color: #ffffffa0;
+        }
+    }
 
     .collapse {
         box-sizing: border-box;
